0bad946f974339223b00de801e6fc157ddf97ac9
Anastasia Kulesh <kulesh.anastasia.s@gmail.com>
2025-03-02 12:41:11 +0300

    fix: fix edit form

--- src/components/event-form/EventForm.tsx
+++ src/components/event-form/EventForm.tsx
@@ -1,6 +1,6 @@
 import styles from "./EventForm.module.css";
 
-import { FormEvent, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import { nanoid } from "nanoid";
 
 export type TEvent = {
@@ -11,68 +11,102 @@ export type TEvent = {
 
 export type TEventFormProps = {
     curEvent?: TEvent | null;
+    clearCurEvent: (data: TEvent | null) => void;
     handlerAddEvent: (data: TEvent) => void;
     handleEditEvent: (data: TEvent) => void;
 };
 export const EventForm = (props: TEventFormProps) => {
-    const { curEvent, handleEditEvent, handlerAddEvent } = props;
+    const { curEvent, clearCurEvent, handleEditEvent, handlerAddEvent } = props;
+    const [eventData, setEventData] = useState({
+        id: "",
+        name: "",
+        date: "",
+    });
 
-    const [eventData, setEventData] = useState<TEvent>(
-        curEvent ? curEvent : { id: "", name: "", date: "" }
-    );
+    useEffect(() => {
+        if (curEvent) {
+            setEventData({
+                id: curEvent.id,
+                name: curEvent.name,
+                date: curEvent.date,
+            });
+        } else {
+            setEventData({
+                id: "",
+                name: "",
+                date: "",
+            });
+        }
+    }, [curEvent]);
 
     const handlerSubmit = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
 
         if (curEvent == null) {
-            setEventData({ ...eventData, id: nanoid(10) });
-            handlerAddEvent(eventData);
-            console.log("in add");
+            handlerAddEvent({
+                id: nanoid(10),
+                name: eventData.name,
+                date: eventData.date,
+            });
         } else {
-            handleEditEvent(eventData);
-            console.log("in edit");
+            handleEditEvent({
+                id: curEvent.id,
+                name: eventData.name,
+                date: eventData.date,
+            });
         }
+        setEventData({ id: "", name: "", date: "" });
+        clearCurEvent(null);
     };
 
     return (
         <div>
-            <h2 className={styles.title}></h2>
             <div className={styles.container}>
-                <form name="addEvent" onSubmit={handlerSubmit}>
-                    <label className={styles.input__field}>
-                        <span className={styles.input__title}>Название</span>
-                        <input
-                            name="name"
-                            type="text"
-                            placeholder="Введите название"
-                            value={eventData.name}
-                            onChange={(event) => {
-                                setEventData({
-                                    ...eventData,
-                                    name: event.target.value,
-                                });
-                            }}
-                            className={styles.input}
-                            required
-                        />
-                    </label>
-                    <label className={styles.input__field}>
-                        Дата проведения
-                        <input
-                            name="date"
-                            type="date"
-                            placeholder="Дата"
-                            value={eventData.date}
-                            onChange={(event) => {
-                                setEventData({
-                                    ...eventData,
-                                    date: event.target.value,
-                                });
-                            }}
-                            className={styles.input}
-                            required
-                        />
-                    </label>
+                <form
+                    name="addEvent"
+                    onSubmit={handlerSubmit}
+                    className={styles.form}
+                >
+                    <div className={styles.input__field__container}>
+                        <label className={styles.input__field}>
+                            <span className={styles.input__title}>
+                                Название
+                            </span>
+                            <input
+                                name="name"
+                                type="text"
+                                placeholder="Введите название"
+                                value={eventData.name}
+                                onChange={(event) => {
+                                    setEventData({
+                                        ...eventData,
+                                        name: event.target.value,
+                                    });
+                                }}
+                                className={styles.input}
+                                required
+                            />
+                        </label>
+                        <label className={styles.input__field}>
+                            <span className={styles.input__title}>
+                                Дата проведения
+                            </span>
+                            <input
+                                name="date"
+                                type="date"
+                                placeholder="Дата"
+                                value={eventData.date}
+                                onChange={(event) => {
+                                    setEventData({
+                                        ...eventData,
+                                        date: event.target.value,
+                                    });
+                                }}
+                                className={styles.input}
+                                required
+                            />
+                        </label>
+                    </div>
                     <button
                         type="submit"
                         title="Сохранить"
