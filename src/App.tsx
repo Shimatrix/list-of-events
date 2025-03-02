15dc9123c16292a41f3042c453258c6fb934e8c0
Anastasia Kulesh <kulesh.anastasia.s@gmail.com>
2025-03-02 12:42:14 +0300

    feat: add layout and pictures

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { EventForm, TEvent } from "./components/event-form/EventForm";
+import { EventList } from "./components/event-list/EventList";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [events, setEvents] = useState<TEvent[]>([]);
+    const [selectedEvent, setSelectedEvent] = useState<TEvent | null>(null);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <div>
+                <h2 className="title">
+                    {selectedEvent == null
+                        ? "Добавление мероприятия"
+                        : "Редактирование мероприятия"}
+                </h2>{" "}
+                <EventForm
+                    curEvent={selectedEvent}
+                    clearCurEvent={setSelectedEvent}
+                    handlerAddEvent={(event) => {
+                        setEvents([...events, event]);
+                    }}
+                    handleEditEvent={(data: TEvent) => {
+                        setEvents(
+                            events.map((item) =>
+                                item.id === data.id ? data : item
+                            )
+                        );
+                    }}
+                ></EventForm>
+                <h1 className="title">Список мероприятий</h1>
+                <EventList
+                    events={events}
+                    handlerEditEvent={setSelectedEvent}
+                    handlerRemoveEvent={(id: string) =>
+                        setEvents(events.filter((item) => item.id !== id))
+                    }
+                ></EventList>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
